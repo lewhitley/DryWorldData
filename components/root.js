5f5e3ec2eb8e551ee4892e1a7550c91086dced64
Lindsey Whitley <lewhitley@gmail.com>
2017-02-22 11:47:00 -0800

    first commit, getting set up done

---
+++ components/root.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Router, Route, hashHistory } from 'react-router';
+
+import App from './app';
+
+const Root = () => {
+  return (
+    <Router history={hashHistory}>
+
+    </Router>
+  );
+};
+
+export default Root;
