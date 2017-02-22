e5575097325b9ab70886f6ba53af56357a4bfbca
Lindsey Whitley <lewhitley@gmail.com>
2017-02-22 15:08:33 -0800

    v1/v2 changes working, styling good

--- components/app.jsx
+++ components/app.jsx
@@ -1,6 +1,159 @@
 import React from 'react';
 import JSONRequest from 'jsonrequest';
 
+const states = [{
+    "name": "Alabama",
+    "capital": "Montgomery"
+  },{
+    "name": "Alaska",
+    "capital": "Juneau"
+  },{
+    "name": "Arizona",
+    "capital": "Phoenix"
+  },{
+    "name": "Arkansas",
+    "capital": "Little Rock"
+  },{
+    "name": "California",
+    "capital": "Sacramento"
+  },{
+    "name": "Colorado",
+    "capital": "Denver"
+  },{
+    "name": "Connecticut",
+    "capital": "Hartford"
+  },{
+    "name": "Delaware",
+    "capital": "Dover"
+  },{
+    "name": "Florida",
+    "capital": "Tallahassee"
+  },{
+    "name": "Georgia",
+    "capital": "Atlanta"
+  },{
+    "name": "Hawaii",
+    "capital": "Honolulu"
+  },{
+    "name": "Idaho",
+    "capital": "Boise"
+  },{
+    "name": "Illinois",
+    "capital": "Springfield"
+  },{
+    "name": "Indiana",
+    "capital": "Indianapolis"
+  },{
+    "name": "Iowa",
+    "capital": "Des Moines"
+  },{
+    "name": "Kansas",
+    "capital": "Topeka"
+  },{
+    "name": "Kentucky",
+    "capital": "Frankfort"
+  },{
+    "name": "Louisiana",
+    "capital": "Baton Rouge"
+  },{
+    "name": "Maine",
+    "capital": "Augusta"
+  },{
+    "name": "Maryland",
+    "capital": "Annapolis"
+  },{
+    "name": "Massachusetts",
+    "capital": "Boston"
+  },{
+    "name": "Michigan",
+    "capital": "Lansing"
+  },{
+    "name": "Minnesota",
+    "capital": "Saint Paul"
+  },{
+    "name": "Mississippi",
+    "capital": "Jackson"
+  },{
+    "name": "Missouri",
+    "capital": "Jefferson City"
+  },{
+    "name": "Montana",
+    "capital": "Helana"
+  },{
+    "name": "Nebraska",
+    "capital": "Lincoln"
+  },{
+    "name": "Nevada",
+    "capital": "Carson City"
+  },{
+    "name": "New Hampshire",
+    "capital": "Concord"
+  },{
+    "name": "New Jersey",
+    "capital": "Trenton"
+  },{
+    "name": "New Mexico",
+    "capital": "Santa Fe"
+  },{
+    "name": "New York",
+    "capital": "Albany"
+  },{
+    "name": "North Carolina",
+    "capital": "Raleigh"
+  },{
+    "name": "North Dakota",
+    "capital": "Bismarck"
+  },{
+    "name": "Ohio",
+    "capital": "Columbus"
+  },{
+    "name": "Oklahoma",
+    "capital": "Oklahoma City"
+  },{
+    "name": "Oregon",
+    "capital": "Salem"
+  },{
+    "name": "Pennsylvania",
+    "capital": "Harrisburg"
+  },{
+    "name": "Rhode Island",
+    "capital": "Providence"
+  },{
+    "name": "South Carolina",
+    "capital": "Columbia"
+  },{
+    "name": "South Dakota",
+    "capital": "Pierre"
+  },{
+    "name": "Tennessee",
+    "capital": "Nashville"
+  },{
+    "name": "Texas",
+    "capital": "Austin"
+  },{
+    "name": "Utah",
+    "capital": "Salt Lake City"
+  },{
+    "name": "Vermont",
+    "capital": "Montpelier"
+  },{
+    "name": "Virginia",
+    "capital": "Richmond"
+  },{
+    "name": "Washington",
+    "capital": "Olympia"
+  },{
+    "name": "West Virginia",
+    "capital": "Charleston"
+  },{
+    "name": "Wisconsin",
+    "capital": "Madison"
+  },{
+    "name": "Wyoming",
+    "capital": "Cheyenne"
+  }
+];
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -17,13 +170,13 @@ class App extends React.Component {
   }
 
   getInfo () {
-    if (this.props.topic === "world" && this.props.version === "v1" || this.props.version === "v2") {
+    if (this.props.topic === "world" && (this.props.version === "v1" || this.props.version === "v2")) {
       return this.fetch('https://restcountries.eu/rest/v1/all');
     } else if (this.props.topic === "national") {
       if (this.props.version === "v1") {
-        return this.fetch('https://restcountries.eu/rest/v1/all');
+        return states;
       } else if (this.props.version === "v2") {
-        return this.fetch('https://restcountries.eu/rest/v1/all');
+        return states;
       }
     }
   }
@@ -34,8 +187,7 @@ class App extends React.Component {
         <ul className='index'>
           {this.getInfo().map( (info, idx) => (
               <li className='index-item' key={idx}>
-                { info.name }
-                // { info.description }
+                <p>{ this.props.version === "v2" && this.props.topic === "national" ? info.capital : info.name }</p>
               </li>
           ))}
         </ul>
--- components/root.js
+++ components/root.js
@@ -7,7 +7,7 @@ const Root = () => {
   return (
     <Router history={hashHistory}>
       <Route path="/v1/topics/national" component={() => (<App version="v1" topic="national"/>)}/>
-      <Route path="/v2/topics/national" component={App} />
+      <Route path="/v2/topics/national" component={() => (<App version="v2" topic="national"/>)} />
       <Route path="/v1/topics/world" component={() => (<App version="v1" topic="world"/>)} />
       <Route path="/v2/topics/world" component={() => (<App version="v2" topic="world"/>)} />
     </Router>
